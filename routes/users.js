8b9295e02710b52dfdceb2d3f8e0340119c171cd
youssef-eissa <youssefeissa1812@gmail.com>
2023-11-19 05:17:50 +0200

    updates

--- routes/users.js
+++ routes/users.js
@@ -23,7 +23,7 @@ router.get('/users', (req, res) => {
 router.get('/users/:username', (req, res) => {
     const found = usersData.some(user => user.username === req.params.username)
     if(found){
-        res.json(usersData.filter(user => user.username === req.params.id))
+        res.json(usersData.filter(user => user.username === req.params.username))
     }else{
         res.status(400).json({msg:`no user with id ${req.params.id}`})
     }
